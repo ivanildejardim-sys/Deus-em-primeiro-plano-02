b830392f223dc74568d95e5e9f03d2b2d8566c85
ivanildejardim-sys <ivanilde.jardim@aluno.senai.br>
2025-12-08 17:02:58 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,23 @@
+// let idade = 18;
+// if(idade >= 18) {
+//     console.log("É maior de idade!");
+// } else if(idade > 15) {
+//     console.log("Aborrecente");
+// } else {
+//     console.log("É menor, mas não é aborrecente!");
+// } 
+// for (let i = 1; i <= 5; i++) {
+//     console.log("número: ", + 1); 
+// } 
+let contador = 0;
+while (contador < 3) {
+    console.log("Numero: ", contador);
+    contador++;
+    }
+    function cumprimentar(nome) {
+        return "olá, " + nome + "1";    
+    }
+    let cumprimento = cumprimentar("Ana");
+    console.log(cumprimento);
+
+ 
